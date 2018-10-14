1968fab61eabe0bb4cf0f31bc3aac5f0dec73f2b
Adrien Maranville <Adrien@Maranville.IO>
2018-10-14 12:03:10 -0600

    wrapping it all up

--- src/components/App.vue
+++ src/components/App.vue
@@ -8,10 +8,30 @@
     <button class="btn btn-success"
             @click="addJoke">Get One More Joke
     </button>
+    <div class="my-2">
+      <div class="col-8 offset-2 col-md-4 offset-md-4">
+
+        <div class="row">
+          <div v-for="type in types"
+               class="col-4 checkboxRow">
+            <label>
+              <input
+                type="checkbox"
+                :value="type"
+                v-model="checkedTypes"
+                checked>
+              <br>
+              {{type}}
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <div class="col-md-12">
       <Joke
         v-for="(joke, index) in $store.state.jokes"
+        v-show="checkedTypes.includes(joke.type)"
         :joke="joke"
         :key="index"
         :index="index"
@@ -25,6 +45,12 @@
   import Joke from './Joke';
 
   export default {
+    data() {
+      return {
+        types: ['general', 'knock-knock', 'programming'],
+        checkedTypes: ['general', 'knock-knock', 'programming']
+      }
+    },
     methods: mapActions([
       'initJokes',
       'addJoke'
